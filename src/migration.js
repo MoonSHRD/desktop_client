087ba2fa44627af2fec1a6210a49c3423b034c6b
Nikita <n.metelkin@madcrush.ru>
2018-08-10 17:10:56 +0300

    get rid of electron db

--- src/migration.js
+++ src/migration.js
@@ -1,16 +1,16 @@
-const db = require('electron-db');
-
-const tables = [
-    "account",
-    "users",
-    "user_messages",
-    "chats",
-    "chat_users",
-];
-
-tables.forEach(function(table) {
-    db.createTable(table, (succ, msg) => {
-        console.log(`Table "${table}" creation success: ${succ}, with message: ${msg}`);
-    });
-});
+// const db = require('electron-db');
+//
+// const tables = [
+//     "account",
+//     "users",
+//     "user_messages",
+//     "chats",
+//     "chat_users",
+// ];
+//
+// tables.forEach(function(table) {
+//     db.createTable(table, (succ, msg) => {
+//         console.log(`Table "${table}" creation success: ${succ}, with message: ${msg}`);
+//     });
+// });
 
