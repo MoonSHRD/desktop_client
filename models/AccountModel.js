c42051b120653b34a79580e52939dbf5c77ccc04
Koristo <nekit01031996@mail.ru>
2018-08-13 14:16:47 +0300

    Add sql in AccountModel

--- models/AccountModel.js
+++ models/AccountModel.js
@@ -1,23 +1,39 @@
-const db = require('electron-db');
+let sql = require('sql.js');
+const file_buffer = fs.readFileSync('../databases/account.sqlite');
+let db = new sql.Database(file_buffer);
 
 class AccountModel {
-    static exit_programm (id) {
+    static exit_programm () {
         console.log('Goodbye!');
+        // Insert callback to server - user out
         close();
-        return true // если успешно вышел
+    };
+
+    static log_in () {
+        if (check_user() === true) {
+
+        }
+        else {
+            console.log("Sorry, we can't find you. Please register");
+            this.registration();
+        }
+    };
+
+    static registration () {
+
     };
 
     static get_contacts (id) {
+        let stmt = db.prepare("SELECT * FROM contacts WHERE id=:id_val");
+        stmt.bind([id]);
         let list_contacts = [];
-        return list_contacts
+        while (stmt.step()) list_contacts.push(stmt.get());
+        return list_contacts // Will return array with arrays with values
     };
 
-    static get_own_profile (my_id) {
-        return {
-            first_name: 'Вася',
-            second_name: 'Пупкин',
-            address: '0xertt54bw35y54by44w'
-        }
+    static get_own_profile () {
+        let res = db.exec("SELECT * FROM info");
+        return res[0].values[0] // Will return array with values
     };
 
     static get_groups (id) {
