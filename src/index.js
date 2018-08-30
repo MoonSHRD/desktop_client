f1288265c91d5d6bfa99e884f0c1aedba015d1b3
Koristo <nekit01031996@mail.ru>
2018-08-30 13:27:21 +0300

    Completely made image compression

--- src/index.js
+++ src/index.js
@@ -80,116 +80,55 @@ window.onload = function () {
         obj.forEach(function (elem) {
             prof[elem.name] = elem.value;
         });
-
-        const file = $(this).find('[name=avatar]').prop('files')[0];
-        if (file) {
-            let reader = new FileReader();
-            reader.onloadend = function () {
-                prof.avatar = reader.result;
-                ipcRenderer.send('submit_profile', prof);
-            };
-            reader.readAsDataURL(file);
-        } else {
-            ipcRenderer.send('submit_profile', prof);
-        }
+        prof.avatar = $("#avatar_preview").attr("src");
+        console.log("Msg1", prof);
+        ipcRenderer.send('submit_profile', prof);
     });
 
     $(document).on('change', '[name=avatar]', function () {
         const file = this.files[0];
+        let fileType = file.type;
         if (file) {
             let reader = new FileReader();
-            // let fileinput = document.getElementById('avatar_preview');
-            // let max_width = fileinput.getAttribute('data-maxwidth');
-            // let max_height = fileinput.getAttribute('data-maxheight');
-            // console.log(max_width, max_height);
-            //
-            reader.onload = function () {
-                //     var blob = new Blob([event.target.result]);
-                //     window.URL = window.URL || window.webkitURL;
-                //     var blobURL = window.URL.createObjectURL(blob);
-                //     // console.log(reader.result);
-                //     // Create and initialize two canvas
-                //     var canvas = document.createElement("canvas");
-                //     var ctx = canvas.getContext("2d");
-                //     let img = new Image();
-                //     img.src = blobURL;
-                //
-                //
-                //     // var canvasCopy = document.createElement("canvas");
-                //     // var copyContext = canvasCopy.getContext("2d");
-                //
-                //
-                //     // let image = document.getElementById('avatar_preview').innerHTML = reader.result;
-                //     // let file = document.getElementById('avatar_preview');
-                //     // file = file.files[0];
-                //     // Create original image
-                //
-                //     let width = img.width;
-                //     let height = img.height;
-                //     console.log(width, height);
-                //     // console.log('old img', img);
-                //     // let max_height = 128;
-                //     // let max_width = 128;
-                //
-                //     // Determine new ratio based on max size
-                //     // var ratio = 1;
-                //     if (width > height) {
-                //         if (width > max_width) {
-                //             //height *= max_width / width;
-                //             height = Math.round(height *= max_width / width);
-                //             width = max_width;
-                //         }
-                //     } else {
-                //         if (height > max_height) {
-                //             //width *= max_height / height;
-                //             width = Math.round(width *= max_height / height);
-                //             height = max_height;
-                //         }
-                //     }
-                //
-                //     // Copy and resize second canvas to first canvas
-                //     canvas.width = width;
-                //     canvas.height = height;
-                //     ctx.drawImage(img, 0, 0, width, height);
-                //     console.log('Code:', canvas.toDataURL());
-                //     $('#avatar_preview').attr('src', canvas.toDataURL("image/jpeg", 0.3));
-                //     $('#avatar_preview').show();
-
-                let imageType = "image/jpg";
-                let imageArguments = 0.7;
-                let newWidth = 128;
-
-                // Create a temporary image so that we can compute the height of the downscaled image.
-                let image = new Image();
+             reader.onloadend = function () {
+                var image = new Image();
                 image.src = reader.result;
-                // console.log(image.src);
+                image.onload = function() {
+                    var maxWidth = 100,
+                        maxHeight = 100,
+                        imageWidth = image.width,
+                        imageHeight = image.height;
 
-                let oldWidth = image.width;
-                let oldHeight = image.height;
-                // let oldWidth = 640;
-                // let oldHeight = 480;
-                console.log('Old size:', oldHeight, oldWidth);
-                let newHeight = Math.floor(oldHeight / oldWidth * newWidth);
+                    if (imageWidth > imageHeight) {
+                        if (imageWidth > maxWidth) {
+                            imageHeight *= maxWidth / imageWidth;
+                            imageWidth = maxWidth;
+                        }
+                    }
+                    else {
+                        if (imageHeight > maxHeight) {
+                            imageWidth *= maxHeight / imageHeight;
+                            imageHeight = maxHeight;
+                        }
+                    }
+                    var canvas = document.createElement('canvas');
+                    canvas.width = imageWidth;
+                    canvas.height = imageHeight;
 
-                // Create a temporary canvas to draw the downscaled image on.
-                let canvas = document.createElement("canvas");
-                canvas.width = newWidth;
-                canvas.height = newHeight;
-                console.log(newHeight, newWidth);
-                // Draw the downscaled image on the canvas and return the new data URL.
-                let ctx = canvas.getContext("2d");
-                ctx.drawImage(image, 0, 0, newWidth, newHeight);
-                let newDataUrl = canvas.toDataURL();
-                console.log('new', newDataUrl);
-                $('#avatar_preview').attr('src', newDataUrl);
-                $('#avatar_preview').show();
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(this, 0, 0, imageWidth, imageHeight);
+                    // The resized file ready for upload
+                    var finalFile = canvas.toDataURL(fileType);
+                    $('#avatar_preview').attr('src', finalFile);
+                    // console.log($("#avatar_preview").);
+                    $('#avatar_preview').show();
+                }
             };
             reader.readAsDataURL(file);
         }
     });
 
     $(document).on('click', '.menuBtn', function () {
-        // var options = { to: { width: 200, direction: "right"} };
         $('.dialogs').toggleClass('resize1', 400);
 
         $('.icon-bar').toggleClass('resize', 400);
@@ -216,9 +155,7 @@ window.onload = function () {
             group: false,
             time: `${date.getHours()}:${date.getMinutes()}`
         };
-        // $('.messaging_history ul').append(`<li class="outMessage">time\n${obj.message}</li>`);
         msg_input.val('');
-        // console.log(obj.message);
 
         ipcRenderer.send("send_message", obj)
     });
@@ -233,19 +170,12 @@ window.onload = function () {
     });
 
     ipcRenderer.on('received_message', (event, obj) => {
-        // console.log(obj);
-        // arg = {
-        //     from:jid,
-        //     message:
-        //     group:false
-        // }
         if ($('.active_dialog').attr('id') === obj.jid) {
             $('.messaging_history ul').append(obj.message);
         }
     });
 
     ipcRenderer.on('buddy', (event, obj) => {
-        // console.log(obj);
         const chat_box = $('.chats ul');
         const user = chat_box.find('#' + obj.address);
         if (user.length) {
@@ -270,7 +200,6 @@ window.onload = function () {
 
     $(document).on('click', '[data-name=join_channel]', function () {
         let active_dialog = $('.active_dialog');
-        // console.log({id:active_dialog.attr('id'),domain:active_dialog.attr('data-domain')});
         ipcRenderer.send('join_channel', {id:active_dialog.attr('id'),domain:active_dialog.attr('data-domain')});
     });
 
