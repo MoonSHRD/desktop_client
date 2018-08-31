f62efd414f4feb070b6ccfbad3a2a23defdba492
a_vigriyanov <a_vigriyanov@madcrush.ru>
2018-08-31 16:10:10 +0500

    transfer files

--- src/index.js
+++ src/index.js
@@ -47,34 +47,7 @@ window.onload = function () {
         // console.log('autyh');
         $('#view').html(arg);
         $.html5Translate(dict, 'en');
-        var offset = 0;
-        var height = parseInt($('.sliderImage').attr('width'));
-        var items = $('.element').length
 
-        $('.sliderWrapper').css('height', height + 60)
-
-        $('.right').click(function(){
-            if(offset > -((items * 100) - 300)){
-                offset = offset - 80;
-                $('.activeNearby').next('.element').addClass('activeNearby');
-                $('.activeNearby').prev('.element').removeClass('activeNearby');
-
-            }
-
-            $('.slider').css('left', offset+'px')
-        })
-
-        $('.left').click(function(){
-            if(offset < 0){
-                offset = offset + 80;
-
-                $('.activeNearby').prev('.element').addClass('activeNearby');
-                $('.activeNearby').next('.element').removeClass('activeNearby');
-
-            }
-
-            $('.slider').css('left', offset+'px')
-        })
     });
 
     // $(document).on('click', '#generate_mnemonic', function () {
