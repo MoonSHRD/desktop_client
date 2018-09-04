c1d4025bde9261de7056a9ed78257e53ba6bba8b
a_vigriyanov <a_vigriyanov@madcrush.ru>
2018-09-04 14:15:30 +0500

    add offer notify

--- src/index.js
+++ src/index.js
@@ -354,5 +354,7 @@ window.onload = function () {
         console.log(`Suggest: ${text.val()}`);
         text.val('');
         $(".send").removeAttr("hidden");
+        $(".alert").fadeIn(200).show();
+        $(".alert").delay(2000).fadeOut(200);
     })
 };
