031ff45d4500f3aaf1bd6565d484ccb56b4e4254
a_vigriyanov <a_vigriyanov@madcrush.ru>
2018-09-05 16:01:15 +0500

    time style update

--- src/router.js
+++ src/router.js
@@ -24,8 +24,8 @@ let acc_data = {
     jidhost: 'localhost',
     privKey: undefined,
     // host: '142.93.226.135',
-    // host: '192.168.1.60',
-    host: 'localhost',
+    host: '192.168.1.5',
+    // host: 'localhost',
     port: 5222,
 };
 
