e22d55650e51fcce045d228e289495b39d71eb44
Nikita <n.metelkin@madcrush.ru>
2018-09-30 18:53:10 +0300

    fixed env conf

--- src/example.env_config.js
+++ src/example.env_config.js
@@ -3,7 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
     jidhost: 'localhost',
     // host				: '142.93.226.135',
-    host: '192.168.1.60',
+    host: 'jackal.moonshrd.io',
+    loom_host: 'node1.moonshrd.io',
+    loom_port: '46658',
+    // net_reg_addr		: '0xc0b5dba954a7efbb1c2e3db228bfba7a0cfd9fa1',
+    net_reg_addr: '0x53bcfb143d7737fedd257a8ba1984872025255fa',
+    token_addr: '0x4dd841b5b4f69507c7e93ca23d2a72c7f28217a8',
     port: 5222,
 };
 //# sourceMappingURL=example.env_config.js.map
