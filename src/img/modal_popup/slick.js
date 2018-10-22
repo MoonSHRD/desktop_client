cb923d11753a4776057165d338e4e4c99399b1c7
AnalniyKorol <lcf-trane@live.ru>
2018-10-22 11:46:26 +0300

    Рефакторим CSS

---
+++ src/img/modal_popup/slick.js
@@ -0,0 +1,33 @@
+// window.jQuery = window.$ = require('jquery');
+// var slick = require('slick-carousel');
+
+//DOM Ready
+$(function() {
+    $('.slick').slick({
+        prevArrow: false,
+        nextArrow: false,
+        infinite: true,
+        // arrows: true,
+        // speed: 300,
+        // centerMode: true,
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+
+    });
+
+    $('#list > li > a').click(function (event) {
+        $(this).parent().children("img").toggleClass('rotate')
+        $(this).parent().children("ul").slideToggle();
+        event.stopPropagation();
+    });
+
+    $('#listGallery > li > a').click(function (event) {
+        $(this).parent().children("img").toggleClass('rotate')
+        $(this).parent().children("ul").slideToggle();
+        event.stopPropagation();
+    });
+
+$('.listOfParticipants').tooltip('enable')
+$('.blockedParticipants').tooltip('enable')
+})
