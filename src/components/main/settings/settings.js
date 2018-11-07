002182dc906dfab509d78710ec105f43e0c5f643
Nonirei <ereikas@gmail.com>
2018-11-07 17:42:50 +0300

    отключен пин, поправлен скролл

--- src/components/main/settings/settings.js
+++ src/components/main/settings/settings.js
@@ -6,31 +6,31 @@ $('[data-name]').on('click', function (e) {
     ipcRenderer.send('change_settings_menu', target);
 });
 
-let pin = '';
-
-$(document).on('click', '.singlePin', function () {
-    console.log(pin);
-    let input = $('input[name="change_pin"]');
-    if(pin.length !== 4) {
-        pin += $(this).children('p').text();
-        input.val(pin);
-        input.change();
-    }
-});
-
-$(document).on('change', 'input[name="change_pin"]', function () {
-    if(pin.length === 4) {
-        if ($(this).val() === '1111') {
-            console.log('Good password');
-            $('#back').css('cssText', 'display: flex !important;');
-            $('#front').css('cssText', 'display: none !important;');
-        } else {
-            console.log('Bad password');
-            pin = '';
-            $(this).val('');
-        }
-    }
-});
+// let pinText = '';
+//
+// $(document).on('click', '.singlePin', function () {
+//     console.log(pinText);
+//     let input = $('input[name="change_pin"]');
+//     if(pinText.length !== 4) {
+//         pinText += $(this).children('p').text();
+//         input.val(pinText);
+//         input.change();
+//     }
+// });
+//
+// $(document).on('change', 'input[name="change_pin"]', function () {
+//     if(pin.length === 4) {
+//         if ($(this).val() === '1111') {
+//             console.log('Good password');
+//             $('#back').css('cssText', 'display: flex !important;');
+//             $('#front').css('cssText', 'display: none !important;');
+//         } else {
+//             console.log('Bad password');
+//             pin = '';
+//             $(this).val('');
+//         }
+//     }
+// });
 
 ipcRenderer.on('change_settings_menu', (event, obj) => {
     $('.settings__right').html(obj);
