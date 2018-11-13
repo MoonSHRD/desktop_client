dbfd61aa1870bc88c97d392c73667288000ca0b0
AnalniyKorol <lcf-trane@live.ru>
2018-11-13 12:53:46 +0300

    Cofig fix

--- src/example.env_config.js
+++ src/example.env_config.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
     jidhost: 'localhost',
     host: 'jackal.moonshrd.io',
+    // host: '192.168.1.60',
+    // host: 'localhost',
     port: 5222,
 };
 exports.ipfs_config = {
@@ -10,20 +12,18 @@ exports.ipfs_config = {
     port: 5001,
 };
 exports.grpc_config = {
-    host: '192.168.1.60',
+    // host: '192.168.1.60',
+    host: '104.248.67.155',
     port: 50051,
 };
 exports.web3_config = {
-    host: "192.168.1.21",
+    // host: "192.168.1.21",
+    host: "104.248.67.155",
     port: 8546,
 };
 exports.loom_config = {
     host: 'node1.moonshrd.io',
     port: 46658,
     net_reg_addr: '0x53bcfb143d7737fedd257a8ba1984872025255fa',
-};
-exports.bot_acc = {
-    privKey: '0x0d84c638556a898cd439d356538318b8a1b0095b6ba5c838c3846b838c4a2461',
-    addr: '0x9869eE8F805BE64Bdba53C5557206F631240b752'.toLowerCase(),
 };
 //# sourceMappingURL=example.env_config.js.map
--- src/example.env_config.ts
+++ src/example.env_config.ts
@@ -1,6 +1,8 @@
 export const config = {
     jidhost: 'localhost',
     host: 'jackal.moonshrd.io',
+    // host: '192.168.1.60',
+    // host: 'localhost',
     port: 5222,
 };
 
@@ -9,13 +11,16 @@ export const ipfs_config = {
     port: 5001,
 };
 
+
 export const grpc_config = {
-    host: '192.168.1.60',
+    // host: '192.168.1.60',
+    host: '104.248.67.155',
     port: 50051,
 };
 
 export const web3_config = {
-    host: "192.168.1.21",
+    // host: "192.168.1.21",
+    host: "104.248.67.155",
     port: 8546,
 };
 
@@ -23,9 +28,4 @@ export const loom_config = {
     host: 'node1.moonshrd.io',
     port: 46658,
     net_reg_addr: '0x53bcfb143d7737fedd257a8ba1984872025255fa',
-};
-
-export const bot_acc={
-    privKey:'0x0d84c638556a898cd439d356538318b8a1b0095b6ba5c838c3846b838c4a2461',
-    addr:'0x9869eE8F805BE64Bdba53C5557206F631240b752'.toLowerCase(),
 };
