d18058b8e7a6af66268c72f2553ca06fd4eec220
Nikita <n.metelkin@madcrush.ru>
2018-12-04 14:00:46 +0300

    исправления для сборки винды, так же внес пару правок по выполнению миграций, добавил иконки, но с ними какая то проблема при сборке, по крайней мере на линуксе, кому то придется разбираться

--- migrations/1543527779604-AddTotalSupplyToChats.ts
+++ migrations/1543527779604-AddTotalSupplyToChats.ts
@@ -4,12 +4,12 @@ export class AddTotalSupplyToChats1543527779604 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         console.log("mgr_up 1");
-        await queryRunner.query(`PRAGMA foreign_keys=OFF`);
+        // await queryRunner.query(`PRAGMA foreign_keys=OFF`);
         await queryRunner.query(`CREATE TABLE "temporary_chat_model" ("id" varchar PRIMARY KEY NOT NULL, "domain" varchar NOT NULL, "name" varchar NOT NULL, "bio" varchar NOT NULL, "avatar" varchar NOT NULL, "role" varchar NOT NULL, "type" varchar NOT NULL, "contract_address" varchar NOT NULL, "unread_messages" integer NOT NULL, "total_supply" varchar)`);
         await queryRunner.query(`INSERT INTO "temporary_chat_model"("id", "domain", "name", "bio", "avatar", "role", "type", "contract_address", "unread_messages", "total_supply") SELECT "id", "domain", "name", "bio", "avatar", "role", "type", "contract_address", "unread_messages", "title" FROM "chat_model"`);
         await queryRunner.query(`DROP TABLE "chat_model";`);
         await queryRunner.query(`ALTER TABLE "temporary_chat_model" RENAME TO "chat_model"`);
-        await queryRunner.query(`PRAGMA foreign_keys=ON`);
+        // await queryRunner.query(`PRAGMA foreign_keys=ON`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
